3a8464466cf82b1ddc1da66b3de8b8df466667c0
JC-28 <83613854+JC-28@users.noreply.github.com>
2023-04-11 21:48:48 -0400

    Add files via upload

--- themes/apostrophe/single-dictionary-entry.php
+++ themes/apostrophe/single-dictionary-entry.php
@@ -40,6 +40,24 @@ get_header(); ?>
             echo '</ul>';
           }
          */ ?>
+         <?php
+          $relatedRecipes = get_field('related_recipes');// array of post objects
+          if($relatedRecipes){
+      
+              echo '<h2 >Related Recipes</h2>';
+              echo '<ul>';
+              foreach($relatedRecipes as $recipes){ //for each a post object
+              ?>
+              <li><a href="<?php echo get_the_permalink($recipes);?>">
+                      <?php echo get_the_title($recipes);?>
+                  </a>
+              </li>   
+      <?php }
+
+          }
+          echo '</ul>';
+      ?>
+         
 			<?php
 				// If comments are open or we have at least one comment, load up the comment template
 			if ( comments_open() ||  get_comments_number() ) :
--- themes/apostrophe/single-recipes.php
+++ themes/apostrophe/single-recipes.php
@@ -13,7 +13,24 @@ get_header(); ?>
 		<?php while ( have_posts() ) : the_post(); ?>
 
 			<?php get_template_part( 'content', 'single' ); ?>
+			<?php
+          $relatedFAQ = get_field('related_questions');// array of post objects
+          if($relatedFAQ){
+      
+              echo '<h2 >Related FAQ</h2>';
+              echo '<ul>';
+              foreach($relatedFAQ as $questions){ //for each a post object
+              ?>
+              <li><a href="<?php echo get_the_permalink($questions);?>">
+                      <?php echo get_the_title($questions);?>
+                  </a>
+              </li>   
+      <?php }
 
+          }
+          echo '</ul>';
+      ?>
+         
 			<?php
 				// If comments are open or we have at least one comment, load up the comment template
 			if ( comments_open() || '0' !== get_comments_number() ) :
